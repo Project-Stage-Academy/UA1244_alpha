72bc55630603625cba41d74dd85f2afd03a7f043
mac <v.kotelnikovdidi@gamil.com>
2024-10-05 10:46:53 +0300

    add startup db model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from users.models import Users
+
+
+class StartUpProfile(models.Model):
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)  # model User is empty
+    name = models.CharField(unique=True, max_length=255, blank=False)
+    description = models.TextField()
+    website = models.CharField(max_length=255, blank=True)
+    startup_logo = models.BinaryField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
