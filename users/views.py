eb861216f22d6df5d67b6fa9208ac590097eb782
PolinaCh14 <chernyshovapolina014@gmail.com>
2024-10-21 13:07:42 +0300

    update: add some logic for logout

--- users/views.py
+++ users/views.py
@@ -6,6 +6,13 @@ from rest_framework import status
 from .models import User
 from .serializers import UserSerializer
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
+from datetime import timedelta
+from datetime import datetime
+
+# import datetime
+from rest_framework_simplejwt.exceptions import TokenError
+from django.core.cache import cache
 
 
 class CustomTokenObtainPairView(TokenObtainPairView):
@@ -33,7 +40,6 @@ class CustomTokenObtainPairView(TokenObtainPairView):
 
         tokens = serializer.validated_data
         user = User.objects.get(email=request.data['email'])
-
         return Response({
             'refresh': str(tokens['refresh']),
             'access': str(tokens['access']),
@@ -62,3 +68,35 @@ class UserProfileView(APIView):
         serializer = UserSerializer(user)
         return Response(serializer.data)
 
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            if refresh_token:
+                token = RefreshToken(refresh_token)
+                token.set_exp(lifetime=timedelta(seconds=0))
+                expiration_time = datetime.fromtimestamp(token["exp"])
+                print(expiration_time)
+            else:
+                return Response({"error": "The refresh token hadn't been provided"}, status=status.HTTP_400_BAD_REQUEST)
+            access_token = request.auth.token
+            try:
+                ac = AccessToken(access_token)
+
+                ac.set_exp(lifetime=timedelta(seconds=0))
+                # cache_key = f"user_{request.user.id}_token"
+                # cache.delete(cache_key)
+                expiration_time = datetime.fromtimestamp(ac["exp"])
+                print(expiration_time)
+                # return Response({"message": "The user logout successful"}, status=status.HTTP_200_OK)
+                response = Response({"message": "User logged out successfully"}, status=status.HTTP_200_OK)
+                response.delete_cookie('access')
+                response.delete_cookie('refresh')
+                return response
+            except TokenError:
+                return Response({"error": "Invalid access token"}, status=status.HTTP_400_BAD_REQUEST)  
+        except Exception as e:
+            return Response({"error":f"{e}"}, status=status.HTTP_400_BAD_REQUEST)
