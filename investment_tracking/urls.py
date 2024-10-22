4d60971a191eb338a0d2fa688286a807a119313b
PolinaCh14 <chernyshovapolina014@gmail.com>
2024-10-22 15:52:45 +0300

    update: change name for save followed startups

--- investment_tracking/urls.py
+++ investment_tracking/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import InvestmentTrackingSaveView
 
 urlpatterns = [
-    path("startup/<int:startup_id>/save", InvestmentTrackingSaveView.as_view(), name = "save-startup")
+    path("startup/<int:startup_id>/save", InvestmentTrackingSaveView.as_view(), name = "save-followed-startups")
 
 ]
