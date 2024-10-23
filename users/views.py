9f4ba2d96fb180e93fcf61a17a235c77136eba49
PolinaCh14 <chernyshovapolina014@gmail.com>
2024-10-23 12:42:25 +0300

    update: went to the finish log-out implementation

--- users/views.py
+++ users/views.py
@@ -8,11 +8,6 @@ from .serializers import UserSerializer
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
 from datetime import timedelta
-from datetime import datetime
-
-# import datetime
-from rest_framework_simplejwt.exceptions import TokenError
-from django.core.cache import cache
 
 
 class CustomTokenObtainPairView(TokenObtainPairView):
@@ -72,34 +67,19 @@ class UserProfileView(APIView):
 class LogoutView(APIView):
     permission_classes = [IsAuthenticated]
 
-    def post(self, request):
-        try:
-            refresh_token = request.data["refresh"]
-            if refresh_token:
-                token = RefreshToken(refresh_token)
-                token.set_exp(lifetime=timedelta(seconds=0))
-                expiration_time = datetime.fromtimestamp(token["exp"])
-                print(expiration_time)
-            else:
-                return Response({"error": "The refresh token hadn't been provided"}, status=status.HTTP_400_BAD_REQUEST)
-            access_token = request.auth.token
-            try:
-                ac = AccessToken(access_token)
+    """
+    View for successfull user log out
+    """
 
-                ac.set_exp(lifetime=timedelta(seconds=0))
-                # ac.delete()
-                # cache_key = f"user_{request.user.id}_token"
-                # cache.delete(cache_key)
-                # expiration_time = datetime.fromtimestamp(ac["exp"])
-                # print(expiration_time)
-                # return Response({"message": "The user logout successful"}, status=status.HTTP_200_OK)
-                response = Response({"message": "User logged out successfully"}, status=status.HTTP_200_OK)
-                # response.delete_cookie('access')
-                # response.delete_cookie('refresh')
-                response.delete_cookie('access_token')
-                response.delete_cookie('refresh_token')
-                return response
-            except TokenError:
-                return Response({"error": "Invalid access token"}, status=status.HTTP_400_BAD_REQUEST)  
-        except Exception as e:
-            return Response({"error":f"{e}"}, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request):
+        refresh_token = request.data["refresh"]
+        if refresh_token:
+            token = RefreshToken(refresh_token)
+            token.set_exp(lifetime=timedelta(seconds=0))
+        else:
+            return Response({"error": "The refresh token hadn't been provided"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        access_token = request.auth.token
+        ac_token = AccessToken(access_token)
+        ac_token.set_exp(lifetime=timedelta(seconds=0))
+        return Response({"message": "User logged out successfully"}, status=status.HTTP_200_OK)
