7dc9e8542050ae8d49de04a2b3ae5134da6e67ef
PolinaCh14 <chernyshovapolina014@gmail.com>
2024-11-01 12:13:53 +0200

    update: add docstring to track_project view

--- projects/views.py
+++ projects/views.py
@@ -10,15 +10,21 @@ from django.shortcuts import get_object_or_404
 
 from .models import Project
 from startups.models import StartUpProfile
-from .serializers import ( ProjectSerializerList,
-                           UpdateProjectSerializer,
-                           CreateProjectSerializer)
+from .serializers import (ProjectSerializerList,
+                          UpdateProjectSerializer,
+                          CreateProjectSerializer)
 
 logger = logging.getLogger("django")
 
 class ProjectsListView(generics.ListAPIView):
     """
-    API view to get all startups projects
+    API view to get all startups' projects.
+
+    Methods:
+        - GET: Retrieves all projects.
+    
+    Returns:
+        - 200 OK: A list of projects.
     """
 
     queryset = Project.objects.all()
@@ -31,41 +37,59 @@ class ProjectsListView(generics.ListAPIView):
         logger.info(f"Fetched {len(response.data)} projects")
         return response
 
+
 class StartupsProjectView(generics.ListAPIView):
     """
-    API view for get list of sturtup's projects
+    API view to get a list of a startup's projects.
+
+    Methods:
+        - GET: Retrieves projects filtered by startup.
+
+    Returns:
+        - 200 OK: A list of the startup's projects.
+        - 404 Not Found: If the startup is not found.
     """
+
     serializer_class = ProjectSerializerList
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        logger.info(f"Fetched sturtup's project")
+        logger.info(f"Fetching startup's projects")
         startup = get_object_or_404(StartUpProfile, id=self.kwargs["startup_id"])
 
         project_list = Project.objects.filter(startup__id=startup.id)
         logger.info(f"Fetched projects for startup {startup.name}")
         return project_list
-    
+
 
 class CreateProjectsView(generics.CreateAPIView):
     """
-    API view for create new project
+    API view to create a new project.
+
+    Methods:
+        - POST: Creates a new project.
+
+    Returns:
+        - 201 Created: If the project is created successfully.
+        - 400 Bad Request: If the provided data is invalid.
+        - 403 Forbidden: If the user does not have permission to create a project for the startup.
     """
+
     serializer_class = CreateProjectSerializer
     permission_classes = [IsAuthenticated]
 
     def create(self, request, *args, **kwargs):
-        logger.info(f"Create project")
+        logger.info(f"Creating project")
         startup = get_object_or_404(StartUpProfile, id=request.data["startup"])
         if startup.user_id.id != request.user.id:
-            logger.error(f"User with id {request.user.id} don't have permission to create project", exc_info=True)
+            logger.error(f"User with id {request.user.id} doesn't have permission to create project", exc_info=True)
             return Response(
-                {"error": "You do not have permission to create new project."},
+                {"error": "You do not have permission to create a new project."},
                 status=status.HTTP_403_FORBIDDEN,
             )
         try:
             response = super().create(request, *args, **kwargs)
-            logger.info(f"Project was created successfully: {response.data["project_id"]}")
+            logger.info(f"Project was created successfully: {response.data['project_id']}")
             return Response(
                 {
                     "message": "New project created successfully",
@@ -80,7 +104,21 @@ class CreateProjectsView(generics.CreateAPIView):
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
+
 class UpdateProjectView(generics.UpdateAPIView):
+    """
+    API view to update an existing project.
+
+    Methods:
+        - PUT/PATCH: Updates the project details.
+
+    Returns:
+        - 200 OK: If the project is updated successfully.
+        - 400 Bad Request: If the provided data is invalid.
+        - 403 Forbidden: If the user does not have permission to update the project.
+        - 404 Not Found: If the project is not found.
+    """
+
     serializer_class = UpdateProjectSerializer
     permission_classes = [IsAuthenticated]
     lookup_field = 'pk'
@@ -89,7 +127,8 @@ class UpdateProjectView(generics.UpdateAPIView):
         return Project.objects.filter(project_id=self.kwargs["pk"])
     
     def update(self, request, *args, **kwargs):
-        logger.info("Update project")
+
+        logger.info("Updating project")
         try:
             project = self.get_object()
         except Http404 as e:
@@ -100,7 +139,7 @@ class UpdateProjectView(generics.UpdateAPIView):
             )
         
         if project.startup.user_id != request.user:
-            logger.error(f"User with this id {request.user.id} don't has permission to update project.", exc_info=True)
+            logger.error(f"User with id {request.user.id} doesn't have permission to update the project.", exc_info=True)
             return Response(
                 {"error": "You do not have permission to update this project."},
                 status=status.HTTP_403_FORBIDDEN
@@ -114,12 +153,10 @@ class UpdateProjectView(generics.UpdateAPIView):
                     "data": response.data,
                 },
                 status=status.HTTP_200_OK,
-                )
+            )
         except ValidationError as e:
             logger.error("Validation error occurred while updating project", exc_info=True)
             return Response(
                 {"error": "Invalid data", "details": e.detail},
                 status=status.HTTP_400_BAD_REQUEST,
             )
-        
-        
