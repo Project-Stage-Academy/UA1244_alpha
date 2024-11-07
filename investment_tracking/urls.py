ecded9a91d447b04b073818fd916b3d89a0a5dc9
0leksandrr <tsybulskyy.sasha@gmail.com>
2024-11-07 19:57:02 +0200

    Merge django migrations to avoid conflicts

--- investment_tracking/urls.py
+++ investment_tracking/urls.py
@@ -5,3 +5,4 @@ urlpatterns = [
     path("startup/<int:startup_id>/save/", InvestmentTrackingSaveView.as_view(), name="save-followed-startups"),
     path("investor/saved-startups/", InvestmentTrackingListView.as_view(), name="list-saved-startups"),
     path("startup/<int:startup_id>/unsave/", InvestmentTrackingUnsaveView.as_view(), name="unsave-followed-startups"),
+]
