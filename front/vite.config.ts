9a9f0cedaa85afdeefe1728b68946a42df4cb492
0leksandrr <tsybulskyy.sasha@gmail.com>
2024-11-11 14:22:19 +0200

    deploy

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -5,7 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',  // Make Vite listen on all network interfaces
-    port: 5173,       // Optional: specify the port if needed
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 5173
   }
 })
